e611da51108a34afb8d2968dc053557d2aaff610
mahmoudsayed235 <mahmoud.sayed235@gmail.com>
2023-04-04 03:44:55 +0200

    sets

---
+++ Into to Python/sets.py
@@ -0,0 +1,9 @@
+set1={"mahmoud" , "ahmed" , "yasser"}
+set2={"taha" , "ahmed" , }
+
+print(set1.difference(set2))
+print(set2.difference(set1))
+print(set1.symmetric_difference(set2))
+print(set1.intersection(set2))
+print(set1.union(set2))
+
